270ea5e713dcdc5402250bf3aabd04bb136f937e
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-02-15 22:27:54 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -25,9 +25,9 @@ def private_panel(_):
             )
         ],
         [
-            InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG"),
+            InlineKeyboardButton(text=_["ST_B_3"], url=f"https://t.me/THE_EXPANDABLES"),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="help_callback hb1")],
     ]
     return buttons
