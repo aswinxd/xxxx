9eb496710676e128482649318458cf0624d0bc20
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-02-27 19:52:25 +0530

    Update __main__.py

--- AnonXMusic/__main__.py
+++ AnonXMusic/__main__.py
@@ -3,18 +3,17 @@ import importlib
 import os
 import sys
 
-from pyrogram import Client, filters
+from pyrogram import Client, idle
+from pyrogram.types import Message
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from AnonXMusic import LOGGER, userbot
+from AnonXMusic import LOGGER, app, userbot
 from AnonXMusic.core.call import Anony
 from AnonXMusic.misc import sudo
 from AnonXMusic.plugins import ALL_MODULES
 from AnonXMusic.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
-
-app = Client("AnonXMusic")
 
 async def init():
     if (
@@ -58,21 +57,24 @@ async def init():
     LOGGER("AnonXMusic").info(
         "\x41\x6e\x6f\x6e\x58\x20\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\n\n\x44\x6f\x6e'\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x46\x61\x6c\x6c\x65\x6e\x41\x73\x73\x6f\x63\x69\x61\x74\x69\x6f\x6e"
     )
+    await idle()
+    await app.stop()
+    await userbot.stop()
+    LOGGER("AnonXMusic").info("Stopping AnonX Music Bot...")
 
-@app.on_message(filters.command("clone") & filters.private)
-async def clone_command(client, message):
+async def clone(update, context):
     # Extract the token from the command arguments
-    if len(message.command) != 2:
-        await message.reply_text("Usage: /clone <token>")
+    if len(context.args) != 1:
+        await update.message.reply_text("Usage: /clone <token>")
         return
-    token = message.command[1]
+    token = context.args[0]
 
     # Set the bot token as environment variable
     os.environ["ANONX_MUSIC_BOT_TOKEN"] = token
 
     # Run initialization
     await init()
-    await message.reply_text("Bot successfully cloned!")
+    await update.message.reply_text("Bot successfully cloned!")
 
 async def main():
     # Check if a token is provided as an argument
@@ -83,4 +85,5 @@ async def main():
     await init()
 
 if __name__ == "__main__":
+    app.add_handler(MessageHandler(clone, filters.command("clone")))
     asyncio.run(main())
